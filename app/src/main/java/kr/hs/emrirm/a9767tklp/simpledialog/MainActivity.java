1169e85cb5a7f8aa45e2c271e01fcb48464579a1
SeMin Jo <a9767tklp@naver.com>
2016-06-07 18:50:14 +0900

    #0 create new project

---
+++ app/src/main/java/kr/hs/emrirm/a9767tklp/simpledialog/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.hs.emrirm.a9767tklp.simpledialog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
